522e046c333799e887aa20bf28b2fe8836fb90e0
georgy-k852 <71917573+georgy-k852@users.noreply.github.com>
2020-09-26 10:53:43 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,2 @@
+admin_id = 543137990
+token = '1384482228:AAHTq4IZ9QwG2TrGC9SOl0MgB2Z0WTheomc'
---
+++ files.py
@@ -0,0 +1,13 @@
+main_db = '/home/tgshop/shop/data/db/main_data.db'
+
+sost_bd = '/home/tgshop/shop/data/bd/sost.bd'
+bot_message_bd = '/home/tgshop/shop/data/bd/bot_message_bd.bd'
+payments_bd = '/home/tgshop/shop/data/bd/payments_bd.bd'
+
+working_log = '/home/tgshop/shop/data/working_log.log'
+
+admins_list = '/home/tgshop/shop/data/lists/admins_list.txt'
+users_list = '/home/tgshop/shop/data/lists/chatid_list.txt'
+blockusers_list = '/home/tgshop/shop/data/lists/blockusers_list.txt'
+
+
